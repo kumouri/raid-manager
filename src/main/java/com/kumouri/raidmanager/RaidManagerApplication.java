f40c1ff004d1ee67d8f0fd8dd11b4ad5e780948b
Will Armstrong <will.c.armstrong@gmail.com>
2020-06-18 15:48:24 -0400

    Initial commit

---
+++ src/main/java/com/kumouri/raidmanager/RaidManagerApplication.java
@@ -0,0 +1,13 @@
+package com.kumouri.raidmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaidManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaidManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kumouri/raidmanager/RaidManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumouri.raidmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaidManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
